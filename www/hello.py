9545d13824eab1aab887f6c4e28a3a4ea83075cd
ios2 <chenyihang520025@126.com>
2017-11-01 18:02:45 +0800

    no message

---
+++ www/hello.py
@@ -0,0 +1,56 @@
+import asyncio
+
+# @asyncio.coroutine
+# def wget(host):
+#     print('wget %s...' % host)
+#     connect = asyncio.open_connection(host, 80)
+#     reader, writer = yield from connect
+#     header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
+#     writer.write(header.encode('utf-8'))
+#     yield from writer.drain()
+#     while True:
+#         line = yield from reader.readline()
+#         if line == b'\r\n':
+#             break
+#         print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
+#     # Ignore the body, close the socket
+#     writer.close()
+
+# loop = asyncio.get_event_loop()
+# tasks = [wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
+# loop.run_until_complete(asyncio.wait(tasks))
+# loop.close()
+# @asyncio.coroutine
+# def hello():
+#     print("hello world")
+#     r = yield from asynio.sleep(1)
+#     print('hello again!')
+# #VS 新语法
+# async def hello():
+#     print ("hello world")
+#     r = await asyncio.sleep(1)
+#     print("hello again!")
+import asyncio
+
+from aiohttp import web
+
+async def index(request):
+    await asyncio.sleep(0.5)
+    return web.Response(body=b'<h1>Index</h1>')
+
+async def hello(request):
+    await asyncio.sleep(0.5)
+    text = '<h1>hello, %s!</h1>' % request.match_info['name']
+    return web.Response(body=text.encode('utf-8'))
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    app.router.add_route('GET', '/hello/{name}', hello)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 8000)
+    print('Server started at http://127.0.0.1:8000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
